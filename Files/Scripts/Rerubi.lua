4a5f395df499c6ac0d604e2053ac76cb2d7dbd2c
Linen <94056347+RealLinen@users.noreply.github.com>
2023-08-13 02:33:09 -0400

    Update Rerubi.lua

--- Files/Scripts/Rerubi.lua
+++ Files/Scripts/Rerubi.lua
@@ -2,8 +2,15 @@ if not getgenv().LRubi then
     getgenv().LRubi = loadstring(game:HttpGet('https://scripts.luawl.com/hosted/1175/21146/Rubi.lua'))()
 end
 return getgenv().LRubi 
+-- Version: 0.1
 --[[
 {
+    json = {
+        encode: function(table),
+        decode: function(string)
+    }
     wrap = function(data: table/string, ...)
+        - > metatable [ you can call the table to execute the script ]
+          globals: {} -- editable
 }
 ]]
