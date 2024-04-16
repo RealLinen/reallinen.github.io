c3dc56f3100d8661dc07cccb65aab85ec02aa619
Linen <94056347+RealLinen@users.noreply.github.com>
2024-04-16 16:55:03 -0400

    Create hookmt.lua

---
+++ Files/Scripts/hookmt.lua
@@ -0,0 +1,69 @@
+-- Github: https://github.com/RealLinen
+-- Discord: linenhs
+newcclosure = typeof(newcclosure) == "function" and newcclosure or (function(...) return ... end)
+local function Initialize(name)
+    name = type(name) == "string" and name or "hookmt"
+    
+end
+
+-- : Middle Man
+local __indexHook = function(isexploit, defaultvalue, Self, Key)
+    -- defaultvalue: function
+    -- isexploit: boolean, checks if call was from exploit or not
+    if isexploit then
+        --print("Aye", Self:GetFullName(), Key)
+    end
+    return defaultvalue()
+end
+local __newindexHook = function(isexploit, defaultvalue, Self, Key, Value)
+    -- defaultvalue: function
+    -- isexploit: boolean, checks if call was from exploit or not
+    if isexploit then
+        --print(Self, Key, Value)
+    end
+    return defaultvalue()
+end
+local __namecallHook = function(isexploit, defaultvalue, Self, namecallmethod, ...)
+    -- defaultvalue: function
+    -- isexploit: boolean, checks if call was from exploit or not
+    local args = {...}
+    --[[
+        Remote:FireServer("hello", "hi")
+        Self = Remote [ Instance ]
+        FireServer = namecallmethod
+        "hello" and "hi" = args [ They're just inside of tables, so like {"hello", "hi"} ]
+        ------------- Remember, this is just an example. Scripts in games will fire with different args/arguments or different namecallmethod with different Instances
+    ]]
+    return defaultvalue()
+end
+
+
+-- Hooking rblox calls
+getgenv()["hookmt"] = getgenv()["hookmt"] or {}
+getgenv()["hookmt"].oldIndex = getgenv()["hookmt"].oldIndex or hookmetamethod(game, "__index", newcclosure(function(...)
+    local Args = {...}
+    local defaultvalue = newcclosure(function()
+        return getgenv()["hookmt"].oldIndex(unpack(Args))
+    end)
+    return newcclosure((getgenv()["hookmt"]["__index"] or __indexHook))((checkcaller or function() return false end)(), defaultvalue, ...)
+end))
+getgenv()["hookmt"].oldNewIndex = getgenv()["hookmt"].oldNewIndex or hookmetamethod(game, "__newindex", newcclosure(function(...)
+    local Args = {...}
+    local defaultvalue = newcclosure(function()
+        return getgenv()["hookmt"].oldNewIndex(unpack(Args))
+    end)
+    return newcclosure((getgenv()["hookmt"]["__newindex"] or __newindexHook))((checkcaller or function() return false end)(), defaultvalue, ...)
+end))
+getgenv()["hookmt"].oldNamecall = getgenv()["hookmt"].oldNamecall or hookmetamethod(game, "__namecall", newcclosure(function(Self, ...)
+    local Args = {...}
+    local defaultvalue = newcclosure(function()
+        return getgenv()["hookmt"].oldNamecall(Self, unpack(Args))
+    end)
+    return newcclosure((getgenv()["hookmt"]["__namecall"] or __namecallHook))((checkcaller or function() return false end)(), defaultvalue, Self, (getnamecallmethod or function() return "" end)(), ...)
+end))
+
+-- Finalize Hooks, you can customize this by making ur own functions
+getgenv()["hookmt"]["__index"] = __indexHook
+getgenv()["hookmt"]["__newindex"] = __newindexHook
+getgenv()["hookmt"]["__namecall"] = __namecallHook
+return Initialize
