73b905afc9470d869ac29beaa6607529ba6430c2
Linen <94056347+RealLinen@users.noreply.github.com>
2024-04-17 18:56:45 -0400

    Update getgchook.lua
    Fixed all bugs

--- Files/Scripts/getgchook.lua
+++ Files/Scripts/getgchook.lua
@@ -36,42 +36,62 @@ local formatupv = function(v)
     return #v
 end
 
-for i,v in next, getgc(true) do
-    if typeof(v) == "function" then
-        local info = debug.info(v, "n")
-        local source = debug.info(v, "s")
-        if source == "" or source == "[C]" or info == ""  then 
-            continue; 
-        end
-        -----------------------
-        local upvals = formatupv(debug.getupvalues(v))
-        getgchook["OnFunction"](info, debug.info(v, "f"), upvals)
-        if getgchook["Hooks"][info] then
+local function search()
+    for i,v in next, getgc(true) do
+        if typeof(v) == "function" then
+            local info = debug.info(v, "n")
+            local source = debug.info(v, "s")
+            if source == "" or source == "[C]" or info == ""  then 
+                continue; 
+            end
+            -----------------------
+            local upvals = formatupv(debug.getupvalues(v))
+            local func = debug.info(v, "f")
+            getgchook["OnFunction"](info, func, upvals)
+
+            -- Basically hook everything so u can modify
             if not getgchook["Cached"][info] then
-                getgchook["Cached"][info] = hookfunc(v, function(...)
-                    getgchook["Hooks"][info] = type(getgchook["Hooks"][info]) == "function" and getgchook["Hooks"][info] or function (old, ...)
-                        return old(...)
-                    end
-                    return getgchook["Hooks"][info](getgchook["Cached"][info], ...)
+                pcall(function()
+                    getgchook["Cached"][info] = hookfunc(func, function(...)
+                        getgchook["Hooks"][info] = type(getgchook["Hooks"][info]) == "function" and getgchook["Hooks"][info] or function(old, ...)
+                            return old(...)
+                        end
+                        return getgchook["Hooks"][info](getgchook["Cached"][info], ...)
+                    end)
                 end)
             end
+            cache[source] = typeof(cache[source]) == "string" and cache[source] or ""
+            cache[source] ..= (string.format("    -  %s | Upvalues: %s", info, upvals)).."\n"
         end
-        cache[source] = typeof(cache[source]) == "string" and cache[source] or ""
-        cache[source] ..= (string.format("    -  %s | Upvalues: %s", info, upvals)).."\n"
     end
-end
-for i, v in next, cache do 
-    str ..= string.format("%s ->\n%s", i, v)
-end
-getgchook["OnSearchEnded"](str)
 
+    for i, v in next, cache do 
+        str ..= string.format("%s ->\n%s", i, v)
+    end
+    
+    getgchook["OnSearchEnded"](str)
+end
+
+getgchook["search"] = search
+search()
 -- Usage [ After Executing, also u can re-execute this as many times as u want ]:
 --[[
     getgchook["OnFunction"] = function(name, func, upvalues_amount)
         -- Each function that the GC Dump finds, it will pass it through here [ can be used for hooking games with ammo, etc ]
     end
+    getgchook.search() -- this is so OnFunction can get called with all the found functions
+
     getgchook["OnSearchEnded"] = function(results)
         -- After the whole GC is dumped, it will copy the results to ur clipboard
         --setclipboard(results)
+    end
+
+    -- Hooks
+    getgchook["Hooks"]["Reload"] = function(old, ...)
+        local args = {...}
+        print(...) -- Print the arguments passed with the function
+        -- to modify something, lets say args[1] was the new amount of bullets to reload to. We would simply do this to have inf ammo:
+        -- args[1] = math.huge 
+        return old(unpack(args)) -- Return the default value. 'old' is custom and not apart of the arguments, its a function used to return the spoofed values
     end
 ]]
