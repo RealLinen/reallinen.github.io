91aa7c47a5af1548c3a0a30eb0b6641b37f60ad5
Linen <94056347+RealLinen@users.noreply.github.com>
2024-04-17 03:17:06 -0400

    Update hookmt.lua

--- Files/Scripts/hookmt.lua
+++ Files/Scripts/hookmt.lua
@@ -6,6 +6,14 @@ local function Initialize(name)
     
 end
 
+
+-- Bypass Adonsis Anti-Cheat
+for k,v in pairs(getgc(true)) do 
+    if pcall(function() return rawget(v, "indexInstance") end) and type(rawget(v,"indexInstance")) == "table" and (rawget(v,"indexInstance"))[1] == "kick" then 
+        v.tvk = {"kick", function() return game.Workspace:WaitForChild("") end} 
+    end 
+end
+
 -- : Middle Man
 local __indexHook = function(isexploit, defaultvalue, Self, Key)
     -- defaultvalue: function
